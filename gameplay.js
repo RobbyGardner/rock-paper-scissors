3c5f04c1019fbdd28517960ed888738dfeff8941
Robby <robbygardner@gmail.com>
2022-07-12 14:27:14 -0700

    Create objects in order to keep score

--- gameplay.js
+++ gameplay.js
@@ -1,69 +1,91 @@
-function computerPlay() {
+//play full game (5 rounds)
+//start with computer and player at 0 score
+//play a round, which console.logs the result of 1 round and the resulting score
+//play another round, resetting computer and player selections
+//console.log new result and updated score
+//repeat for 5 cycles
+//console.log final score and result of game
+
+function computerChoose() {
   //randomly return rock paper scissors
-  let computerPick = Math.floor(Math.random() * 3) + 1;
-  if (computerPick === 1) {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
     return "rock";
-  } else if (computerPick === 2) {
+  } else if (randomNumber === 2) {
     return "paper";
-  } else return "scissors";
-  console.log(computerPick);
+  } else {
+    return "scissors";
+  }
 }
 
-const playerPrompt = prompt("Type rock, paper, or scissors");
-
-function playerSelection() {
-  if (playerPrompt != null) playerSelection = playerPrompt.toLowerCase();
+function playerChoose() {
+  let playerPrompt = prompt("Type rock, paper, or scissors");
+  if (playerPrompt != null) {
+    return playerPrompt.toLowerCase();
+  }
 }
 
-function playRound(playerSelection, computerSelection) {
-  let playerScore = 0;
-  let computerScore = 0;
+function playRound() {
+  let playerSelection = playerChoose();
+  let computerSelection = computerChoose();
+
   if (playerSelection === "paper" && computerSelection === "rock") {
     console.log("You win! Paper beats rock");
-    playerScore++;
-    console.log(playerScore, computerScore);
+    playerObj.score = playerObj.score + 1;
+    console.log(playerObj.score, computerObj.score);
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     console.log("You lose! Scissors beats paper");
-    computerScore++;
-    console.log(playerScore, computerScore);
+    computerObj.score = computerObj.score + 1;
+    console.log(playerObj.score, computerObj.score);
   } else if (playerSelection === "paper" && computerSelection === "paper") {
     console.log("Tie! Go again");
-    console.log(playerScore, computerScore);
+    console.log(playerObj.score, computerObj.score);
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     console.log("You win! Scissors beats paper");
-    playerScore++;
-    console.log(playerScore, computerScore);
+    playerObj.score = playerObj.score + 1;
+    console.log(playerObj.score, computerObj.score);
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     console.log("You lose! Rock beats scissors");
-    computerScore++;
-    console.log(playerScore, computerScore);
+    computerObj.score = computerObj.score + 1;
+    console.log(playerObj.score, computerObj.score);
   } else if (
     playerSelection === "scissors" &&
     computerSelection === "scissors"
   ) {
     console.log("Tie! Go again");
-    console.log(playerScore, computerScore);
+    console.log(playerObj.score, computerObj.score);
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     console.log("You win! Rock beats scissors");
-    playerScore++;
-    console.log(playerScore, computerScore);
+    playerObj.score = playerObj.score + 1;
+    console.log(playerObj.score, computerObj.score);
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     console.log("You lose! Paper beats rock");
-    computerScore++;
-    console.log(playerScore, computerScore);
+    computerObj.score = computerObj.score + 1;
+    console.log(playerObj.score, computerObj.score);
   } else if (playerSelection === "rock" && computerSelection === "rock") {
     console.log("Tie! Go again");
-    console.log(playerScore, computerScore);
+    console.log(playerObj.score, computerObj.score);
   }
 }
 
-function game() {
+function playFullGame() {
+  playerObj.score = 0;
+  computerObj.score = 0;
+
   for (let i = 0; i < 5; i++) {
     playRound();
   }
-  if (playerScore > computerScore) {
+  if (playerObj.score > computerObj.score) {
     console.log("You are the winner!");
-  } else if (playerScore === computerScore) {
-    console.log("It's a tie!");
-  } else console.log("Computer is the winner!");
+  } else if (playerObj.score < computerObj.score) {
+    console.log("Computer is the winner!");
+  } else console.log("It's a tie!");
 }
+
+const playerObj = {
+  score: 0,
+};
+
+const computerObj = {
+  score: 0,
+};
